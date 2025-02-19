4262ac783cc3f715734d3284370cb85eea2a70d3
Swathi Madhavan <swathipattam123@gmail.com>
2025-02-19 21:29:36 +0530

    Added sidebar and the layout

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,16 +1,36 @@
 import { Box } from "@mui/material";
 import React from "react";
 import { LayoutProps } from "../model";
+import SideBar from "../components/sidebar";
 
-
-
-function Layout({children}: Readonly<LayoutProps>){
-    return (
-        <Box>
-
-        </Box>
-    );
+function Layout({ children }: Readonly<LayoutProps>) {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#10141F",
+        display: "flex",
+        flexDirection: {
+          xs: "column",
+          lg: "row",
+        },
+        color: "white",
+        padding: 3,
+        gap: 3,
+        overflowY: "hidden",
+        height: "100vh",
+      }}
+    >
+      <SideBar />
+      <Box
+        sx={{
+          width: "100%",
+          overflowY: "scroll",
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
 }
-
 
 export default Layout;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Layout from "../../Layout";
+import { Box, Paper } from "@mui/material";
+
 
 function Home() {
-  return <div>Home</div>;
+  return <Layout><Box>
+    <Paper component="form"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            borderRadius: "default",
+            p: 1,
+            backgroundColor: "#10141f",
+            border: "none",
+          }}>
+            
+    </Paper>
+    </Box>
+    </Layout>;
 }
 
 export default Home;
